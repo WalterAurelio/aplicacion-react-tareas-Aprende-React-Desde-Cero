a784e3db37008e4384268755875b83dbada43c5d
Aurelio Gareca <aurelio.gareca.94@hotmail.com>
2024-01-09 11:59:04 -0300

    Se definió el componente TareaFormulario y sus estilos.

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -2,14 +2,14 @@ import React from 'react';
 import '../hojas-de-estilo/Tarea.css';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 
-function Tarea({ texto }) {
+function Tarea({ texto, completada }) {
   return (
-    <div className='tarea-contenedor'>
+    <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
       <div className='tarea-texto'>
         {texto}
       </div>
-      <div className='tarea-icono'>
-        <AiOutlineCloseCircle className='tarea-icono'/>
+      <div className='tarea-contenedor-iconos'>
+        <AiOutlineCloseCircle className='tarea-icono' /> 
       </div>
     </div>
   );
