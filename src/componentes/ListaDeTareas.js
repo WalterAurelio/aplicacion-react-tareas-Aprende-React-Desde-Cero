6a4ec00151f0c2caa258671769c286bcaddf7cc5
Aurelio Gareca <aurelio.gareca.94@hotmail.com>
2024-01-10 16:25:46 -0300

    Se añadieron funcionalidades a los componentes TareaFormulario y ListaDeTareas.

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -4,17 +4,33 @@ import '../hojas-de-estilo/ListaDeTareas.css';
 import Tarea from './Tarea';
 
 function ListaDeTareas() {
-  const [tareas, setTareas] = useState([]); /* Un arreglo vacío será el estado inicial de tareas. */
+  const [tareas, setTareas] = useState([]);
+
+  const agregarTarea = (tarea) => {
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
+  };
+
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  }
 
   return (
     <>
-      <TareaFormulario />
+      <TareaFormulario onSubmit={agregarTarea} />
       <div className='tareas-lista-contenedor'>
         {
           tareas.map((tarea) => 
             <Tarea
+              key={tarea.id}
+              id={tarea.id}
               texto={tarea.texto}
               completada={tarea.completada}
+              eliminarTarea={eliminarTarea}
             />
           )
         }
